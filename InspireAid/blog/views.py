b1611e556fc7cc8fbe914c5b29aad43fbebdffad
SuyashaAdhikariComputing <np01cp4s220020@islingtoncollege.edu.np>
2024-02-16 18:32:07 +0545

    third

--- InspireAid/blog/views.py
+++ InspireAid/blog/views.py
@@ -6,7 +6,9 @@ from blog.models import Post
 def bloghome(request):
     allPosts=Post.objects.all()
     context= {'allPosts': allPosts}
-    return render(request,'blog/bloghome.html')
+    return render(request,'blog/bloghome.html',context)
 
 def blogpost(request, slug):
-    return HttpResponse(f'This is bloghome: {slug}')
+    post= Post.objects.filter (slug=slug).first()
+    context={'post': post}
+    return render(request,'blog/blogPost.html',context)
