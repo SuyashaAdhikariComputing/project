16417d2fe2f92bb8a59713976da23b7938beadfd
SuyashaAdhikariComputing <np01cp4s220020@islingtoncollege.edu.np>
2024-02-24 16:26:48 +0545

    tenth

--- InspireAid/blog/urls.py
+++ InspireAid/blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('createblog/',views.postcontent, name='postcontent'),
     path('<str:slug>/',views.blogpost, name='blogpost'),
     path('editblog/<slug:slug>/', views.edit_post, name='editblog'),
-    path('deleteblog/<slug:slug>/', views.deleteblog, name='deleteblog'),
+    path('deleteblog/<slug:slug>/', views.deleteblog, name='deleteblog'),#path
 ]
