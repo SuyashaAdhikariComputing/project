f605f4297ed8fe078455199c6429f739cb378773
SuyashaAdhikariComputing <np01cp4s220020@islingtoncollege.edu.np>
2024-03-03 19:26:50 +0545

    eleventh

--- InspireAid/campaign/views.py
+++ InspireAid/campaign/views.py
@@ -12,4 +12,14 @@ class CampaignDetailView(DetailView):
     model=Campaign
     template_name='campaign/campaignDetail.html'
 
-    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get the current campaign object
+        current_campaign = self.get_object()
+
+        # Get three recommended campaigns (you may customize this logic based on your requirements)
+        recommended_campaigns = Campaign.objects.exclude(pk=current_campaign.pk)[:3]
+
+        context['recommended_campaigns'] = recommended_campaigns
+        return context
