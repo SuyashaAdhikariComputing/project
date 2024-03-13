484807327bb7f32ee0172571f053a1e46799a8c9
SuyashaAdhikariComputing <np01cp4s220020@islingtoncollege.edu.np>
2024-03-13 15:24:43 +0545

    fourtheen

--- InspireAid/campaign/urls.py
+++ InspireAid/campaign/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path, include
-from . views import CampaignView,CampaignDetailView
-
+from . views import CampaignView,CampaignDetailView, CampaignPostView,CampaignEditView
+from campaign import views
 urlpatterns = [
     path('campaignhome/',CampaignView.as_view(),name="campaignhome"),
+    path('createcampaign/',CampaignPostView.as_view(), name='postcampaign'),
     path('<slug:slug>/', CampaignDetailView.as_view(), name='campaigndetail'),
+    path('editcampaign/<slug:slug>/', CampaignEditView.as_view(), name='campaignedit'),
+    path('deletecampaign/<slug:slug>/', views.deletecampaign, name='campaigndelete'),
 ]
--- InspireAid/campaign/views.py
+++ InspireAid/campaign/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
 
+from pyexpat.errors import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, View, DeleteView
 from .models import Campaign
 # Create your views here.
 
@@ -23,3 +25,50 @@ class CampaignDetailView(DetailView):
 
         context['recommended_campaigns'] = recommended_campaigns
         return context
+    
+class CampaignPostView(View):
+
+    template_name = 'campaign/campaignform.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        # Retrieve data from POST request
+        title = request.POST.get('title')
+        description = request.POST.get('content')
+        target_amount = request.POST.get('amount')
+
+        # Create Campaign object
+        campaign = Campaign.objects.create(
+            title=title,
+            description=description,
+            target_amount=target_amount
+        )
+
+        # Redirect to success URL
+        return redirect('/campaign/campaignhome/')
+    
+class CampaignEditView(View):
+    template_name = 'campaign/campaignform.html'
+
+    def get(self, request, slug, *args, **kwargs):
+        campaign = get_object_or_404(Campaign, slug=slug)
+        return render(request, self.template_name, {'campaign': campaign})
+
+    def post(self, request, slug, *args, **kwargs):
+        campaign = get_object_or_404(Campaign, slug=slug)
+
+        campaign.title = request.POST.get('title')
+        campaign.description = request.POST.get('content')
+        campaign.target_amount = request.POST.get('amount')
+        campaign.save()
+
+        return redirect('/campaign/campaignhome/')
+
+
+def deletecampaign(request, slug):
+    campaign = get_object_or_404(Campaign, slug=slug)
+    campaign.delete()
+    
+    return redirect('campaignhome')
